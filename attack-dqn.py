2d7f876662daba9949620bb8e11a7e62cd4f6739
haider4445 <haiderrana45@gmail.com>
2021-07-17 12:58:06 -0400

    attack-dqn-functions

--- attack-dqn.py
+++ attack-dqn.py
@@ -14,21 +14,9 @@ import os
 import sys
 import time
 from strategy import Strategy
+from parser import parser
 
 start_time_program = time.time()
-
-
-parser = argparse.ArgumentParser(description = "Fast Undetectable Attack")
-parser.add_argument('-mp','--Path', metavar = 'path', type = str, help = 'Complete path to model')
-parser.add_argument('-e','--env', type = str, nargs = "?", default = "PongNoFrameskip-v4", help = 'Environment name like PongNoFrameskip-v4')
-parser.add_argument('-p','--perturbationType', nargs="?", default="rfgsm", type = str, help = 'Perturbation Type: fgsm, rfgsm, cw, optimal')
-parser.add_argument('-a', '--attack', nargs="?", default=1, type = int, help = 'Attack 1 or not to attack 0')
-parser.add_argument('--stepsRFGSM', nargs = "?", default = 1, type = int, help = "Number of steps of RFGSM attack")
-parser.add_argument('--alphaRFGSM', nargs = "?", default = 8/255, type = float, help = "Alpha (Step Size) of RFGSM attack")
-parser.add_argument('--epsRFGSM', nargs = "?", default = 16/255, type = float, help = "Epsilon (strength) of RFGSM attack")
-parser.add_argument('--totalgames', nargs = "?", default = 10, type = int, help = "total games/episodes")
-parser.add_argument('--strategy', nargs = "?", default = "allSteps", type = str, help = "Attack strategy: random, allSteps, leastSteps, critical")
-parser.add_argument('--targeted', nargs = "?", default = 0, type = int, help = "0 or 1")
 
 
 args = parser.parse_args()
@@ -133,6 +121,7 @@ while Numberofgames != TotalGames:
 
 						if strategy == "critical":
 
+							rfgsmIns = CW(model = net, targeted = targeted)
 							adv_state = rfgsmIns.forward(state_v,orig_action_tensor)
 							for i in range(len(adv_acts)):
 								print("Attacking...")
